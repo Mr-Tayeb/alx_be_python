712c39858269ce866277601f38ee6632db5090c9
Mr-Tayeb <abijoue.ta@gmail.com>
2025-07-26 22:12:08 +0100

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,7 +6,7 @@ def perform_operation(num1, num2, operation):
     elif operation == "multiply":
         return num1 * num2
     else operation == "divide":
-        if num2 = 0:
+        if num2 == 0:
             return "Cannot devid by 0!"
         else:
             return num1 / num2
